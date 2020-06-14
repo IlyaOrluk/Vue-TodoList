e1511394b7ac7b6886373a2574705ad8b04af53d
IlyaOrluk <misfits120@mail.ru>
2020-06-14 23:32:05 +0300

    init

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <li>{{ todo.title }}</li>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        todo: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <ul>
+            <TodoItem 
+                v-for="todo in todos"
+                :key="todo.id"
+                v-bind:todo="todo"
+            />
+        </ul>
+    </div>
+</template>
+<script>
+  import TodoItem from '@/components/TodoItem'
+
+  export default {
+    props: ['todos'],
+    components: {
+      TodoItem
+    }
+  }
+</script>
